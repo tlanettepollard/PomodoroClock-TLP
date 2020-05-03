a46603a38afa4ec7380936ce569035a63e271f51
T. Lanette <TLPollard4516@gmail.com>
2020-05-03 17:22:16 -0400

    Added header and footer
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/App.js
+++ pomodoroclock-tlp/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import '../src/styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <h1 className="appTitle">Rebel-Cow Pomodoro Clock</h1>
+        </div>
+        
+        <div className="footer">
+          <h4 className="footer-attribute">
+            React JS Pomodoro Clock App created by {" "} <a href="https://github.com/TLanetteRose">{" "} <span> T.Lanette Pollard </span></a> &nbsp;
+            FreeCodeCamp Front End Libraries Project
+          </h4>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
