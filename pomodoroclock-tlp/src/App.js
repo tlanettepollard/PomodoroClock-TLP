ebdfbfc65711498afe041aeec3c6151430450547
T. Lanette <TLPollard4516@gmail.com>
2020-05-03 18:39:10 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/App.js
+++ pomodoroclock-tlp/src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import '../src/styles/App.css';
 
+//import Timer from './components/Timer/Timer';
+//import Controllers from './components/Controllers/Controllers';
+//import Sound from './components/Sound/Sound';
 export default class App extends Component {
   render() {
     return (
---
+++ pomodoroclock-tlp/src/components/Settings/Settings.js
@@ -0,0 +1,44 @@
+//Break and Session
+
+import React, {Component} from 'react';
+
+
+export default class Settings extends Component {
+    handleSessionIncrement = () => {
+        this.props.sessionIncrement()
+    }
+
+    handleSessionDecrement = () => {
+        this.props.sessionDecrement()
+    }
+
+    handleBreakIncrement = () => {
+        this.props.breakIncrement()
+    }
+
+    handleBreakDecrement = () => {
+        this.props.breakDecrement()
+    }
+
+
+    render() {
+        return (
+            <div className="settings">
+                <div className="settings-section">
+                    <label id="session-label">Session Length</label>
+                    <div>
+                        <button className="btn-setting" id="sesson-increment" on click={this.handleSessionIncrement}>+</button>
+                        <span id="session-length">{this.props.workTime}</span>
+                        <button className="btn-setting" id="session-decrement" onClick={this.handleSessionDecrement}>-</button>
+                    </div>
+
+                </div>
+                <div className="settings-section">
+                    <label id="break-label">Break Length</label>
+                    
+                </div>  
+            </div>
+        
+        )
+    }
+}
