42c617491162dea21a7f21ef69609ed673d22ebe
T. Lanette <TLPollard4516@gmail.com>
2020-05-04 19:16:27 -0400

    Outlined structure of App
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/App.js
+++ pomodoroclock-tlp/src/App.js
@@ -9,12 +9,22 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="header">
+        <div className="header-container">
           <h1 className="app-title">Rebel-Cow Pomodoro Clock</h1>
+        </div>
+        <div className="timer-container">
+          /TIMER/
+        </div>
+        <div className="controller-container">
+          /WORK/
+          /BREAK/
+        </div>
+        <div className="sound-container">
+          /SOUND/
         </div>
         <div className="footer">
           <h3 className="footer-attribute">
-            React JS Calculator App created by {" "} <a href="https://github.com/TLanetteRose"> {" "} <span> T.Lanette Pollard </span></a> &nbsp;
+            React JS Pomodoro Clock App created by {" "} <a href="https://github.com/TLanetteRose"> {" "} <span> T.Lanette Pollard </span></a> &nbsp;
             FreeCodeCamp Front End Libraries Project
           </h3>
         </div>
