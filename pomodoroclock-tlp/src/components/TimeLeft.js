0224f1093615593c30c36e438762063a05ce4f3b
T. Lanette <TLPollard4516@gmail.com>
2020-05-09 19:37:31 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/components/TimeLeft.js
+++ pomodoroclock-tlp/src/components/TimeLeft.js
@@ -1,23 +1,46 @@
-import moment from 'moment';
-import momentDurationFormatSetup from 'moment-duration-format';
-import React from 'react';
-import {useState} from 'react';
+//import moment from 'moment';
+//import momentDurationFormatSetup from 'moment-duration-format';
+import React, {Component} from 'react';
+//import {useState} from 'react';
 
-momentDurationFormatSetup(moment);
+//momentDurationFormatSetup(moment);
 
-const TimeLeft = ({sessionLengthSeconds}) => {
+/*const TimeLeft = ({sessionLengthSeconds}) => {
     const [timeLeft] = useState(sessionLengthSeconds);
 
-    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss');
+    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss');*/
 
-    
-    return (
+    /*return (
       <div className="time-content">
         <label id="timer-label">Timer</label>
         <span id="time-left">{formattedTimeLeft}</span>
       </div>
     );
-};
+};*/
 
-export default TimeLeft;
+const formatTime = (timeLeftInSecond) => {
+  let minute = Math.floor(timeLeftInSecond / 60);
+  if (minute < 10) minute = '0' + minute;
+
+  let second = timeLeftInSecond - 60 * minute;
+  if (second < 10) second = '0' + second;
+
+  return `${minute}:${second}`;
+}
+
+export default class TimeLeft extends Component {
+  render() {
+    return (
+      <div className="time-content">
+        <label id="timer-label">{this.props.timeLabel}</label>
+        <span id="time-left">{formatTime(this.props.timeLeftInSecond)}</span>
+      </div>
+    )
+  }
+}
+
+
+
+
+
 
