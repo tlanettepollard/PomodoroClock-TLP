e88bcd8089f2da4b0908f19c88a6bb12d64942f7
T. Lanette <TLPollard4516@gmail.com>
2020-05-08 22:01:39 -0400

    Updated Session and Break
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/components/Break.js
+++ pomodoroclock-tlp/src/components/Break.js
@@ -1,33 +1,21 @@
-import React, {useState} from 'react';
+import moment from 'moment';
+import React from 'react';
 
-const Break = () => {
-    const[
-        breakLengthSeconds,
-        setBreakLengthSeconds
-    ] = useState(300); //seconds for 5 minutes
-
-    const decrementBreakLength = () => {
-        const newBreakLength = breakLengthSeconds - 60;
-        if (newBreakLength < 0){
-            setBreakLengthSeconds(0);
-        }else {
-            setBreakLengthSeconds(newBreakLength);
-        }
-    };
-    const incrementBreakLength = () => {
-        setBreakLengthSeconds(
-            breakLengthSeconds + 60
-        );
-    }
+const Break = ({
+    breakLengthSeconds,
+    incrementBreakLength,
+    decrementBreakLength,
+    }) => {
+    const breakLengthMinutes = moment.duration(breakLengthSeconds, "s").minutes(); //seconds to minutes conversion
 
     return (
         <div>
             <p id="break-label">Break</p>
-            <p id="break-length">{breakLengthSeconds}</p>
+            <p id="break-length">{breakLengthMinutes}</p>
             <button id="break-increment" onClick={incrementBreakLength}>+</button>
             <button id="break-decrement" onClick={decrementBreakLength}>-</button>
         </div>
     );
-}
+};
 
 export default Break;
--- pomodoroclock-tlp/src/components/Session.js
+++ pomodoroclock-tlp/src/components/Session.js
@@ -1,5 +1,21 @@
-import React, {Component} from "react";
+import moment from "moment";
+import React from "react";
  
-export default class Session extends Component {
+const Session = ({
+    sessionLengthSeconds,
+    incrementSessionLength,
+    decrementSessionLength
+}) => {
+    const sessionLengthMinutes = moment.duration(sessionLengthSeconds, 's').minutes();
+    
+    return (
+        <div>
+            <p id="session-label">Session</p>
+            <p id="session-length">{sessionLengthMinutes}</p>
+            <button id="session-increment" onClick={incrementSessionLength}>+</button>
+            <button id="session-decrement" onClick={decrementSessionLength}>-</button>
+        </div>
+    );
+};
 
-}
+export default Session;
