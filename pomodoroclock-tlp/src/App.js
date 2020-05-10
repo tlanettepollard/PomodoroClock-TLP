bfea6bb75d1e22c241e9e55c433d45b9c9fc2534
T. Lanette <TLPollard4516@gmail.com>
2020-05-09 21:15:50 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/App.js
+++ pomodoroclock-tlp/src/App.js
@@ -77,8 +77,63 @@ export default class App extends Component {
        isStart: false,
        timerInterval: null
      });
+     this.audioBeep.current.pause();
+     this.audioBeep.current.currentTime = 0;
+     this.state.timerInterval && clearInterval(this.state.timerInterval);
    }
+   onStartStop() {
+     if (!this.state.isStart) {
+       this.setState({
+         isStart: !this.state.isStart,
+         timerInterval: setInterval(() => {
+           this.decreaseTimer();
+           this.phaseControl();
+         }, 1000)
+       })
+     } else {
+       this.audioBeep.current.pause();
+       this.audioBeep.current.currentTime = 0;
+       this.state.timerInterval && clearInterval(this.state.timerInterval);
 
+       this.setState({
+         isStart: !this.state.isStart,
+         timerInterval: null
+       });
+     }
+   }
+decreaseTimer() {
+  this.setState({
+    timeLeftInSecond: this.state.timeLeftInSecond - 1
+  });
+}
+
+phaseControl() {
+  if (this.state.timeLeftInSecond === 0) {
+    this.audioBeep.current.play();
+  } else if (this.state.timeLeftInSecond === -1) {
+    if (this.state.timeLabel === 'Session') {
+      this.setState({
+        timeLabel: 'Break',
+        timeLeftInSecond: this.state.breakLength * 60
+      });
+    } else {
+      this.setState({
+        timeLabel: 'Session',
+        timeLeftInSecond: this.state.sessionLength * 60
+      });
+    }
+  }
+}
+
+render() {
+  return (
+    <div className="container">
+      <div className="head-container">
+        <h1 className="app-title"> Rebel-Cow Pomodoro Clock </h1>
+      </div>
+    </div>
+  )
+}
 
 }
 
