cd93a9a36c3ec7709c7787511755f96787708cdf
T. Lanette <TLPollard4516@gmail.com>
2020-05-09 20:16:57 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/components/Controllers.js
+++ pomodoroclock-tlp/src/components/Controllers.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 export default class Controller extends Component {
     render() {
+        
         return (
             <div className="controller">
-                <button className="controller-btn" id="start_stop" onClick={this.props.onStartStop}> Start/Stop
+                <button className="controller-btn" id="start_stop" onClick={this.props.onStartStop}>
                     {this.props.isStart ? 'Stop' : 'Start'}
                     <i className="fa fa-play fa-2x"></i>
                     <i className="fa fa-pause fa-2x"></i>
                 </button>
-                <button className="controller-btn" id="reset" onClick={this.props.onReset}> Reset
+                <button className="controller-btn" id="reset" onClick={this.props.onReset}> 
                    <i className="fa fa-refresh fa-2x"></i>
                 </button>
             </div>
