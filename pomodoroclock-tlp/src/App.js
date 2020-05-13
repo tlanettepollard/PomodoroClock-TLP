a2e645b3585698b8d80b6b313700d6c8b03921d3
T. Lanette <TLPollard4516@gmail.com>
2020-05-13 18:45:05 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/App.js
+++ pomodoroclock-tlp/src/App.js
@@ -8,6 +8,41 @@ import soundfile from './michigan_frog.wav';
 
 const App = () => {
   const myAudio = useRef();
+  const context = new AudioContext();
+  const [breakLengthSeconds, setBreakLength]= useState(5);
+  const [sessionLengthSeconds, setSessionLength]= useState(25);
+  const [timerLabel, setTimerLabel]= useState('Session');
+  const [timeLeft, setTimeLeft]= useState(25 * 60);
+  const [timerRunning, setTimerRunning]= useState(false);
+
+  const incrementSessionLength = () => {
+    if (!timerRunning && sessionLengthSeconds < 60){
+      setSessionLength(sessionLengthSeconds + 1)
+      setTimeLeft((sessionLengthSeconds + 1) * 60);
+    }
+  }
+  const decrementSessionLength = () => {
+    if(!timerRunning && sessionLengthSeconds > 1){
+      setSessionLength(sessionLengthSeconds - 1)
+      setTimeLeft((sessionLengthSeconds - 1) * 60);
+    }
+  }
+  const incrementBreakLength = () => {
+    if(!timerRunning && breakLengthSeconds < 60){
+      setBreakLength(breakLengthSeconds + 1)
+    }
+  }
+  const decrementBreakLength = () => {
+    if(!timerRunning && breakLengthSeconds > 1){
+      setBreakLength(breakLengthSeconds - 1)
+    }
+  }
+
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+
+
 
 
 
