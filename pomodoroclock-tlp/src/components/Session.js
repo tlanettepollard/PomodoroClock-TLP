b956855b2ee8bb1b30901a759cb33adba46b6a35
T. Lanette <TLPollard4516@gmail.com>
2020-05-13 17:27:38 -0400

    Updates and Redos
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/src/components/Session.js
+++ pomodoroclock-tlp/src/components/Session.js
@@ -1,55 +1,23 @@
-//import moment from "moment";
-import React from "react";
- 
+import React from 'react';
 
-//DevTo AryanJ Tutorial for commented section
-/*const Session = ({
-    sessionLengthSeconds,
-    incrementSessionLength,
-    decrementSessionLength
-}) => {
-    const sessionLengthMinutes = moment.duration(sessionLengthSeconds, 's').asMinutes();
-    
-    return (
-        <div className="session-container">
-            <p id="session-label">Session</p>
-            <p id="session-length">{sessionLengthMinutes}</p>
-            <div className="session-btn">
-                <button className="session-ctrl-btn" id="session-increment" onClick={incrementSessionLength}>
-                    <i class="fa fa-plus-square"></i>
-                </button>
-                <button className="session-ctrl-btn" id="session-decrement" onClick={decrementSessionLength}>
-                    <i class="fa fa-minus-square"></i>
-                </button>
-            </div>
-        </div>
-    );
-};*/
 const Session = props => {
-
     return (
         <div className="session-container">
-            <h3 id="session-label">Session</h3>
+            <h3 id="session-label">Session Length</h3>
 
             <div className="session-btn">
-                <button id="session-increment" className="session-ctrl-btn" onClick={props.incrementSessionLength}>
-                    <i className="fa fa-plus-square"/>
+                <button id="session-increment" onClick={props.incrementSession}>
+                    <i className="fa fa-plus-square fa-2x"/>
                 </button>
-
                 <h3 id="session-length">
-                    {props.sessionLengthMinutes}
+                    {props.sessionLength}
                 </h3>
-
-                <button id="session-decrement" className="session-ctrl-btn" onClick={props.decrementSessionLength}>
-                    <i className="fa fa-minus-square"/>
+                <button id="session-decrement" onClick={props.decrementSession}>
+                    <i className="fa fa-minus-square fa-2x"/>
                 </button>
             </div>
         </div>
     )
 }
 
-
-
-
-
-export default Session; 
+export default Session;
