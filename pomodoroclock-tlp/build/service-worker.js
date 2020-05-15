66d0dee160b8638afddf3ad41908ddc65ba0cb57
T. Lanette <TLPollard4516@gmail.com>
2020-05-15 16:48:57 -0400

    Update
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- pomodoroclock-tlp/build/service-worker.js
+++ pomodoroclock-tlp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30dda4784207e1626cfaab6d8c47428e.js"
+  "/PomodoroClock-TLP/precache-manifest.b9dba0ed7f347f103767518ba84080ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PomodoroClock-TLP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
